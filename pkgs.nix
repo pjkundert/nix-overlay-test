824b0f973830d4f3f18949db3cef488303ee81d9
Perry Kundert <perry@dominionrnd.com>
2024-12-23 09:13:51 -0700

    Work toward patchelf on Linux

--- pkgs.nix
+++ pkgs.nix
@@ -3,8 +3,8 @@
 import (pkgs.fetchFromGitHub {
   owner = "pjkundert";
   repo = "nix-overlay";
-  rev = "a2bad93f3f37719c1dfbfbbb839f8b828140de38";
-  sha256 = "sha256-z5Xyv2zHLT1EpI9jjA1K89uJdxDGCfrXlbbUXXncCNc=";
+  rev = "67613f9aff8428efe87e0e6cad26547524c5f736";
+  sha256 = "sha256-pI2JZlygJk5gSkdneReCwXpwGmysvngkhYSiuY5zaO8=";
 }) {
   inherit pkgs;
   inherit system;
