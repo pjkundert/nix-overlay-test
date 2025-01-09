0c3566c459a0d4265ed2637f8810475cf30d3d20
Perry Kundert <perry@dominionrnd.com>
2025-01-09 16:20:06 -0700

    Official nix-overlay release

--- pkgs.nix
+++ pkgs.nix
@@ -1,9 +1,9 @@
 { pkgs, system }:
 
 import (pkgs.fetchFromGitHub {
-  owner = "pjkundert";
+  owner = "spartan-holochain-counsel";
   repo = "nix-overlay";
-  rev = "517dbf1e689cc8d78dd3735f27cdecf7362fba28";
+  rev = "5046d25439ee9da1e469a975d26f538622f2f3fc";
   sha256 = "sha256-cUEqTp3wxTeCBBd/dtjuck/1vyR1YXGcimkmd8ZXcRw=";
 }) {
   inherit pkgs;
